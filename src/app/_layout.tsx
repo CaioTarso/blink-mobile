6ab4094403bc3b73ce3f5e4662fd33ff0453f3b9
Daricrys <112405945+Daricrys@users.noreply.github.com>
2026-04-14 00:07:47 -0300

    Revert "Feature/agendamentos cliente"

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,16 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import React from 'react';
+import { useColorScheme } from 'react-native';
 
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+import { AnimatedSplashOverlay } from '@/components/animated-icon';
+import AppTabs from '@/components/app-tabs';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AnimatedSplashOverlay />
+      <AppTabs />
+    </ThemeProvider>
+  );
 }
